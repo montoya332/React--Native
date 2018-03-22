3c055582bb5dd32fe2fe9abe1c17297366cdb4f7
Arturo Montoya <arturo.montoya332@gmail.com>
2018-03-21 21:09:37 -0700

    update view , added eslint

--- helloWorld/screens/HomeScreen.js
+++ helloWorld/screens/HomeScreen.js
@@ -1,218 +1,170 @@
 import React from 'react';
 import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
+	Image,
+	Platform,
+	ScrollView,
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	View,
 } from 'react-native';
 import { WebBrowser } from 'expo';
 import {
-  Location,
-  jobItems,
-  educationItems,
-  projectItems,
-  languageIconItems,
-  skillsIconItems
+	Location,
+	jobItems,
+	educationItems,
+	projectItems,
+	languageIconItems,
+	skillsIconItems
 } from '../assets/stubData/resume';
 import { MonoText } from '../components/StyledText';
 
 export default class HomeScreen extends React.Component {
-  static navigationOptions = {
-    header: null,
-  };
+static navigationOptions = {
+header: null,
+};
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View>
-            <HeroTitle />
-            <Timeline
-              title="Background"
-              headerIcon="fa-clock-o"
-              items={jobItems}
-              pageLine
-            />
-            <Timeline
-              title="Education"
-              headerIcon="fa-rocket"
-              items={educationItems}
-            />
-            <Timeline
-              title="Projects"
-              headerIcon="fa-rocket"
-              items={projectItems}
-            />
-          </View>
-        </ScrollView>
-      </View>
-    );
-  }
+render() {
+return (
+<View style={styles.container}>
+<ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+<View>
+<HeroTitle />
+<Timeline
+title="Background"
+headerIcon="fa-clock-o"
+items={jobItems}
+pageLine
+/>
+<Timeline
+title="Education"
+headerIcon="fa-rocket"
+items={educationItems}
+/>
+<Timeline
+title="Projects"
+headerIcon="fa-rocket"
+items={projectItems}
+/>
+</View>
+</ScrollView>
+</View>
+);
+}
 
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
+_maybeRenderDevelopmentModeWarning() {
+if (__DEV__) {
+const learnMoreButton = (
+<Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
+Learn more
+</Text>
+);
 
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
+return (
+<Text style={styles.developmentModeText}>
+Development mode is enabled, your app will be slower but you can use useful development
+tools. {learnMoreButton}
+</Text>
+);
+} else {
+return (
+<Text style={styles.developmentModeText}>
+You are not in development mode, your app will run at full speed.
+</Text>
+);
+}
+}
 
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
+_handleLearnMorePress = () => {
+WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
+};
 
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
+_handleHelpPress = () => {
+WebBrowser.openBrowserAsync(
+'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
+);
+};
 }
 
 const HeroTitle = props => (
-  <Text>HeroTitle</Text>
+<View>
+<Text>{props.title}</Text>
+<Text>{props.subtitle}</Text>
+</View>
 );
 
 const Timeline = props => (
-  <View>
-    <SectionHeader
-      title={props.title}
-    />
-    { props.items.map((item, key) => <TimelineItem key={key} item={item} pageLine={props.pageLine} />) }
-  </View>
+<View>
+<SectionHeader
+title={props.title}
+/>
+{ props.items.map((item, key) => <TimelineItem key={key} item={item} pageLine={props.pageLine} />) }
+</View>
 );
 
 const TimelineItem = (props) => {
-  const { item, pageLine } = props;
-  return (
-    <View>
-      <Text>{`${item.startDate} - ${item.endDate}`}</Text>
-      <Text>{item.location}</Text>
-      <Text>{props.details}</Text>
-    </View>
-  );
+const { item, pageLine } = props;
+return (
+<View>
+<TimelineItemHeader {...props} />
+<Text>{`${item.startDate} - ${item.endDate}`}</Text>
+<Text>{item.location}</Text>
+<Text>{props.details}</Text>
+</View>
+);
 };
 
 const TimelineItemHeader = props => (
- <Text>TimelineItemHeader</Text>
+<View>
+{props.item.role && (
+<View>
+<Text> {props.item.role} </Text>
+<Text> at </Text>
+</View>
+)}
+<Text> {props.item.title} </Text>
+</View>
 );
 
 const SectionHeader = props => (
-    <View>
-      <Text>{props.title}</Text>
-    </View>
+<View>
+<Text>{props.title}</Text>
+</View>
 );
 
 const TimelineHeader = props => (
- <Text>TimelineHeader</Text>
+<Text>TimelineHeader</Text>
 );
 
 const TimelineMilestoneIcon = (props) => {
- return (<Text>TimelineMilestoneIcon</Text>)
+return (<Text>TimelineMilestoneIcon</Text>)
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+container: {
+flex: 1,
+backgroundColor: '#fff',
+},
+contentContainer: {
+paddingTop: 30,
+},
+/* development */
+developmentModeText: {
+marginBottom: 20,
+color: 'rgba(0,0,0,0.4)',
+fontSize: 14,
+lineHeight: 19,
+textAlign: 'center',
+},
+helpContainer: {
+marginTop: 15,
+alignItems: 'center',
+},
+helpLink: {
+paddingVertical: 15,
+},
+helpLinkText: {
+fontSize: 14,
+color: '#2e78b7',
+},
 });
